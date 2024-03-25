4b87b830f0f26c15b3314544ff0b2bc3c3f9237a
Carl Turechek <carl@siteraiser.com>
2024-03-25 19:16:49 -0400

    Add files via upload

---
+++ models/product/orders.go
@@ -0,0 +1,45 @@
+package productmodel
+
+import (
+	"database/sql"
+	"reflect"
+)
+
+func SubmitTx(
+	user map[string]interface {
+	},
+	tx TxJSON,
+) bool {
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/pong_store")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	//User is the seller's user id
+	if reflect.ValueOf(tx.Ia_id).IsZero() {
+		result, err := db.Exec(`INSERT INTO orders (uuid,user) VALUES(?,?)`, tx.Uuid, user["userid"])
+		if err != nil {
+			panic(err)
+		}
+
+		lastid, err := result.LastInsertId()
+		if err != nil || lastid < 1 {
+			return false
+		}
+
+	} else {
+		result, err := db.Exec(`INSERT INTO orders (uuid,ia_id,user) VALUES(?,?,?)`, tx.Uuid, tx.Ia_id, user["userid"])
+		if err != nil {
+			panic(err)
+		}
+
+		lastid, err := result.LastInsertId()
+		if err != nil || lastid < 1 {
+			return false
+		}
+	}
+
+	return true
+
+}
